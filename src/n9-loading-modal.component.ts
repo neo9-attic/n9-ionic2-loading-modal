38f31a71c777a745c8eab1204fbcb176f2e691a5
Gaetan SENN <gaetan.senn@gmail.com>
2017-05-15 15:10:05 +0200

    scss: Inline style url of component

--- src/n9-loading-modal.component.ts
+++ src/n9-loading-modal.component.ts
@@ -8,7 +8,10 @@ import { N9LoadingModalService } from "./n9-loading-modal.service";
 			<div class="backdrop"></div>
 			<ion-spinner name="dots"></ion-spinner>
 		</div>
-	`
+	`,
+  styleUrls: [
+    'n9-loading-modal.scss'
+  ]
 })
 export class N9LoadingModalComponent {
   public loader: any;
